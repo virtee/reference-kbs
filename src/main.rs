3cc51a8988d204481928b5313224500efccc2a53
Sergio Lopez <slp@redhat.com>
2022-10-07 14:48:01 +0200

    Update the changes of kbs-types
    Adopt the latest changes on the kbs-types crate.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -101,26 +101,27 @@ async fn auth(
 ) -> Result<Value, BadRequest<String>> {
     let session_id = Uuid::new_v4().to_simple().to_string();
 
-    let workload_id = request.workload_id.clone();
-    let tee_config: Option<String> = match db
-        .run(move |conn| {
-            configs::table
-                .filter(configs::workload_id.eq(workload_id))
-                .first::<TeeConfig>(conn)
-        })
-        .await
-    {
-        Ok(e) => Some(e.tee_config),
-        Err(_) => None,
-    };
-
     let mut attester: Box<dyn Attester> = match request.tee {
         Tee::Sev => {
             let sev_request: SevRequest = serde_json::from_str(&request.extra_params)
                 .map_err(|e| BadRequest(Some(e.to_string())))?;
+
+            let workload_id = sev_request.workload_id.clone();
+            let tee_config: Option<String> = match db
+                .run(move |conn| {
+                    configs::table
+                        .filter(configs::workload_id.eq(workload_id))
+                        .first::<TeeConfig>(conn)
+                })
+                .await
+            {
+                Ok(e) => Some(e.tee_config),
+                Err(_) => None,
+            };
+
             Box::new(SevAttester::new(
+                sev_request.workload_id.clone(),
                 session_id.clone(),
-                request.workload_id.clone(),
                 sev_request.build,
                 sev_request.chain,
                 tee_config,
@@ -133,7 +134,7 @@ async fn auth(
         .challenge()
         .map_err(|e| BadRequest(Some(e.to_string())))?;
 
-    let session = Session::new(session_id, request.workload_id.clone(), attester);
+    let session = Session::new(session_id, attester.workload_id().clone(), attester);
     cookies.add(Cookie::new("session_id", session.id()));
 
     state
